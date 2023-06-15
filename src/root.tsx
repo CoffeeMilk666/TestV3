1eb449f6851e866b6651f63cb68efa8dd78794ee
CoffeeMilk666 <134580785+CoffeeMilk666@users.noreply.github.com>
2023-06-15 15:58:04 +0800

    root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -25,8 +25,8 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
+            <A href="/">庭院</A>
+            <A href="/about">後院</A>
             <Routes>
               <FileRoutes />
             </Routes>
